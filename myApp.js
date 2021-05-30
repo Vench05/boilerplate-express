89c64fe2fff9f2d6be1b0e218ff1cb257310f7c6
Vench05 <ralfvenchbillones@gmail.com>
2021-05-30 16:47:42 +0800

    params query string

--- myApp.js
+++ myApp.js
@@ -16,19 +16,38 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html')
 })
 
-app.get('/json', (req, res) => {
-    res.send({"message": 'Hello json'})
-})
-
 // app.get('/json', (req, res) => {
-//     data = "Hello json"
-//     if (process.env.MESSAGE_STYLE === "uppercase") {
-//         res.send({"message": data.toUpperCase()})
-//     }
-//     res.send({"message": data})
+//     res.send({"message": 'Hello json'})
 // })
 
+app.get('/now', (req, res, next) => {
+    const now = new Date
+    req.time = now.toString()
+    next()
+}, (req, res) => {
+    res.send({ "time": req.time })
+})
 
+
+app.get('/:word/echo', (req, res) => {
+    res.send({"echo": req.params.word})
+})
+
+app.get('/json', (req, res) => {
+    let data = "Hello json"
+    if (process.env.MESSAGE_STYLE === "uppercase") {
+        data = data.toUpperCase()
+    }
+    res.send({"message": data})
+})
+
+app.route('/name')
+    .get((req, res) => {
+        res.send({ "name": `${req.query.first} ${req.query.last}`})
+    })
+    .post((req, res) => {
+        res.send({ "name": `${req.query.first} ${req.query.last}`})
+    })
 
 
 
