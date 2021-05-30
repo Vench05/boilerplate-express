f145074f09fa1f2ffc7e2d71e09d05dd8cbed5d2
Vench05 <ralfvenchbillones@gmail.com>
2021-05-30 09:45:45 +0800

    static files

--- myApp.js
+++ myApp.js
@@ -3,10 +3,19 @@ var app = express();
 
 
 
+// middleware
+app.use("/public", express.static(__dirname + "/public"));
 
 
+// console.log("Hello World");
+// app.get('/', function (req, res) {
+//     res.send('Hello Express');
+// })
 
 
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/index.html')
+})
 
 
 
