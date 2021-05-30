da3bdf9f44f00ddddcb85b286f49a85696af3dae
Vench05 <ralfvenchbillones@gmail.com>
2021-05-30 15:14:15 +0800

    middleware logger

--- myApp.js
+++ myApp.js
@@ -1,26 +1,32 @@
 var express = require('express');
 var app = express();
+require('dotenv').config();
 
 
 
 // middleware
 app.use("/public", express.static(__dirname + "/public"));
-
-
-// console.log("Hello World");
-// app.get('/', function (req, res) {
-//     res.send('Hello Express');
-// })
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.path} - ${req.ip}`);
+    next()
+})
 
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html')
 })
 
-
-app.get('/', (req, res) => {
-    res.send({"message": "Hello json"})
+app.get('/json', (req, res) => {
+    res.send({"message": 'Hello json'})
 })
+
+// app.get('/json', (req, res) => {
+//     data = "Hello json"
+//     if (process.env.MESSAGE_STYLE === "uppercase") {
+//         res.send({"message": data.toUpperCase()})
+//     }
+//     res.send({"message": data})
+// })
 
 
 
